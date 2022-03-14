06178b9ceb324f5d039fe7336c6ade2e74808fea
Cram42 <cram42@users.noreply.github.com>
2022-03-14 13:01:52 +0800

    Actual patch

--- NPCMiddleMan/Program.cs
+++ NPCMiddleMan/Program.cs
@@ -5,6 +5,9 @@ using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using System.Threading.Tasks;
+using Mutagen.Bethesda.Plugins;
+using System.IO;
+using Mutagen.Bethesda.Plugins.Records;
 
 namespace NPCMiddleMan
 {
@@ -14,13 +17,45 @@ namespace NPCMiddleMan
         {
             return await SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
-                .SetTypicalOpen(GameRelease.SkyrimSE, "YourPatcher.esp")
+                .SetTypicalOpen(GameRelease.SkyrimSE, "NPCMiddleMan.esp")
                 .Run(args);
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            ModKey easyNpcModKey = new("NPC Appearances Merged", ModType.Plugin);
+            bool easyNpcPatched = state.LoadOrder.PriorityOrder.HasMod(easyNpcModKey);
+            if (!easyNpcPatched) throw new Exception("Not patched by EasyNPC");
+
+            state.PatchMod.MasterReferences.Add(new() { Master = easyNpcModKey });
+
+            ISkyrimModGetter easyNpc = SkyrimMod.CreateFromBinaryOverlay(ModPath.FromPath(Path.Combine(state.DataFolderPath, easyNpcModKey.ToString())), SkyrimRelease.SkyrimSE);
+            foreach(INpcGetter npcGetter in easyNpc.Npcs.Records)
+            {
+                Npc npc = state.PatchMod.Npcs.GetOrAddAsOverride(npcGetter);
+                string raceFormKeyString = npc.Race.FormKey.ToString();
+
+                if (npc.WornArmor.IsNull) continue;
+                IArmorGetter oldArmor = npc.WornArmor.Resolve(state.LinkCache);
+                string currentId = oldArmor.EditorID ?? throw new Exception("No EditorID... weird.");
+
+                Armor newSkin = state.PatchMod.Armors.AddNew($"{currentId}_{npc.EditorID ?? npc.FormKey.ToString()}");
+                newSkin.DeepCopyIn(oldArmor, new Armor.TranslationMask(true) { EditorID = false, Armature = false });
+
+
+                foreach (IFormLinkGetter<IArmorAddonGetter> addonLink in oldArmor.Armature)
+                {
+                    IArmorAddonGetter oldAddon = addonLink.Resolve(state.LinkCache);
+                    if ( (oldAddon.Race.FormKey.ToString() == raceFormKeyString) ||
+                         oldAddon.AdditionalRaces.Select(r => r.FormKey.ToString()).Contains(raceFormKeyString) )
+                    {
+                        newSkin.Armature.Add(oldAddon);
+                        continue;
+                    }
+                }
+
+                npc.WornArmor.SetTo(newSkin);
+            }
         }
     }
 }
